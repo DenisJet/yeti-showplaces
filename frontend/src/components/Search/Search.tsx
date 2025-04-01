7411b2ccf73f4b740b0b68d041ce7a58c891998e
Denis Kalkopf <ddenproton@protonmail.com>
2025-04-01 21:39:43 +0500

    style: fix some styles

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -4,6 +4,9 @@ import './Search.scss';
 import {TextInput} from '@gravity-ui/uikit';
 import {useAppDispatch, useAppSelector} from '@/store/store.hooks';
 import {setSearchQuery} from '@/store/showplaces.slice';
+import block from 'bem-cn-lite';
+
+const b = block('search');
 
 export const Search = () => {
     const {searchQuery} = useAppSelector((state) => state.showplaces);
@@ -20,6 +23,7 @@ export const Search = () => {
             value={searchQuery}
             onUpdate={handleSearch}
             size="m"
+            className={b()}
         />
     );
 };
